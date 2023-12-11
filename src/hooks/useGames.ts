05ed0990947c3d625bcf7b7798a429562de0e0e0
nevaeh <xiny7803@gmail.com>
2023-12-11 22:01:08 +1300

    Display platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,20 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number,
+    name: string,
+    slug: string,
+
+}
+
 export interface Game {
     id: number,
     name: string,
     background_image: string,
+    parent_platforms:{ platform: Platform }[], // An array of objects, with each object having a property called 'platform' of type 'Platform'.
 }
+
 interface FetchGamesResponse {
     count: number,
     results: Game[],
